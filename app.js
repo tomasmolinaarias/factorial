3fabb4f55eaad09a8f95a6177588232ea10b36de
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-10-27 17:41:50 -0300

    listo

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-let factorial_user = prompt("danos un numero del 1 al 20", 1);
-for (i = 1 ; i <= factorial_user ; i++){
-let resultado = factorial_user* i
-document.write([`${i} * ${factorial_user} = ${resultado} <br>`])
-}
-let proceso = factorial_user - 1;
-let factorial_final = factorial_user * proceso
-document.write(`el factorial es ${factorial_final}`) 
-// function factoria(numero) {
-//     if(numero*(numero-1)){
-//     return resultado
-//     console.log(`${resultado}`)
+
+const factorial_user = prompt("danos un numero del 1 al 20", 1);
+//----------------------------------------------------------------
+//tabla de multiplicacion
+// for (i = 1 ; i <= factorial_user ; i++){
+// let resultado = factorial_user* i
+// document.write([`${i} * ${factorial_user} = ${resultado} <br>`])
+// }
+//-------------------------------------------------------------------
+//factorial
+//intento1
+// let proceso = factorial_user - 1;
+// let factorial_final = factorial_user * proceso
+// document.write(`el factorial es ${factorial_final}`) 
+let res = factorial_user;
+//ordenado
+// for (let i = 1; i <= res; i++) { let res = 1;
+//     for (let j= 1; j <= i; j++) {
+//     res = res * j; }
+//       document.write(`FACTORIAL DE ${i} ES : ${res} <br>` )
 //     }
 
-//  }
-//  console.log(factoria(factorial_user))
+ //_---------------------------------------------------
 
+let listo =function(){
+
+  for (i = 1 ; i <= factorial_user ; i++){
+    let resultado = factorial_user* i
+    document.write([`${i} * ${factorial_user} = ${resultado} <br>`])
+    }
+  for (let i = 1; i <= res; i++) { let res = 1;
+    for (let j= 1; j <= i; j++) {
+    res = res * j; }
+      document.write(`FACTORIAL DE ${i} ES : ${res} <br>` )
+    }
+    
+}
+listo()
